7b38a62bd3f48cf66f272ddf6512873d80069fec
unknown <anton13jeejo@gmail.com>
2020-09-24 20:14:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import TinderCards from "./components/TinderCards/TinderCards";
+import SwipeButtons from "./components/SwipeButtons/SwipeButtons";
+import Chats from "./components/Chats/Chats";
+import ChatScreen from "./components/ChatScreen/ChatScreen";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/chat/:person">
+            <Header backButton="/chat" />
+            <ChatScreen />
+          </Route>
+          <Route path="/chat">
+            <Header backButton="/" />
+            <Chats />
+          </Route>
+          <Route path="/">
+            <Header />
+            <TinderCards />
+            <SwipeButtons />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
